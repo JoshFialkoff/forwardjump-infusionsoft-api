f08afc408e2552a320830b38725b3b2b29471aed
Timothy Jensen <tim@timjensen.us>
2017-02-09 13:40:05 -0500

    Bumped version

--- bootstrap.php
+++ bootstrap.php
@@ -18,7 +18,7 @@
  * Text Domain:       fj-infusionsoft-api
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Version:           1.2.4
+ * Version:           1.2.5
  */
 
 // If this file is called directly, abort.
